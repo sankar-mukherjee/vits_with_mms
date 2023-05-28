74d0e3cf99385b8a459d1b41e904f9d49a156058
sankar <sankar1535@gmail.com>
2023-05-28 11:08:43 +0000

    mel+preprocessing restore to old

--- mel_processing.py
+++ mel_processing.py
@@ -11,7 +11,7 @@ import numpy as np
 # from librosa.util import normalize, pad_center, tiny
 from scipy.signal import get_window
 from scipy.io.wavfile import read
-# from librosa.filters import mel as librosa_mel_fn
+from librosa.filters import mel as librosa_mel_fn
 
 MAX_WAV_VALUE = 32768.0
 
@@ -82,34 +82,6 @@ def spec_to_mel_torch(spec, n_fft, num_mels, sampling_rate, fmin, fmax):
     return spec
 
 
-# def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin, fmax, center=False):
-#     if torch.min(y) < -1.:
-#         print('min value is ', torch.min(y))
-#     if torch.max(y) > 1.:
-#         print('max value is ', torch.max(y))
-
-#     global mel_basis, hann_window
-#     dtype_device = str(y.dtype) + '_' + str(y.device)
-#     fmax_dtype_device = str(fmax) + '_' + dtype_device
-#     wnsize_dtype_device = str(win_size) + '_' + dtype_device
-#     if fmax_dtype_device not in mel_basis:
-#         mel = librosa_mel_fn(sampling_rate, n_fft, num_mels, fmin, fmax)
-#         mel_basis[fmax_dtype_device] = torch.from_numpy(mel).to(dtype=y.dtype, device=y.device)
-#     if wnsize_dtype_device not in hann_window:
-#         hann_window[wnsize_dtype_device] = torch.hann_window(win_size).to(dtype=y.dtype, device=y.device)
-
-#     y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
-#     y = y.squeeze(1)
-
-#     spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[wnsize_dtype_device],
-#                       center=center, pad_mode='reflect', normalized=False, onesided=True)
-
-#     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
-
-#     spec = torch.matmul(mel_basis[fmax_dtype_device], spec)
-#     spec = spectral_normalize_torch(spec)
-
-#     return spec
 def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size, fmin, fmax, center=False):
     if torch.min(y) < -1.:
         print('min value is ', torch.min(y))
@@ -128,10 +100,9 @@ def mel_spectrogram_torch(y, n_fft, num_mels, sampling_rate, hop_size, win_size,
 
     y = torch.nn.functional.pad(y.unsqueeze(1), (int((n_fft-hop_size)/2), int((n_fft-hop_size)/2)), mode='reflect')
     y = y.squeeze(1)
-    with torch.amp.autocast(enabled=False):
-        y = y.float()
-        spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[wnsize_dtype_device],
-                        center=center, pad_mode='reflect', normalized=False, onesided=True)
+
+    spec = torch.stft(y, n_fft, hop_length=hop_size, win_length=win_size, window=hann_window[wnsize_dtype_device],
+                      center=center, pad_mode='reflect', normalized=False, onesided=True)
 
     spec = torch.sqrt(spec.pow(2).sum(-1) + 1e-6)
 
